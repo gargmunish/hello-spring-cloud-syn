24a0120aa92a451601bd7ea7adb6c5c71640bd19
gargmunish <42627686+gargmunish@users.noreply.github.com>
2018-10-05 11:53:24 -0500

    Update HomeController.java

--- src/main/java/helloworld/HomeController.java
+++ src/main/java/helloworld/HomeController.java
@@ -3,6 +3,7 @@ package helloworld;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HomeController {
@@ -17,6 +18,22 @@ public class HomeController {
     public String home2(Model model) {
 
         return "home-homecontroller-search";
+    }
+    
+    @RequestMapping("/getFromFoundry")
+    public String home3(Model model) {
+    	RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject("http://springcloud.local.pcfdev.io/search", String.class);
+        result = result+"fromFoundry";
+        return result;
+    }
+    
+    @RequestMapping("/getFromOutoFFoundry")
+    public String home4(Model model) {
+    	RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject("http://192.168.1.69:8082/search", String.class);
+        result = result+"fromoutOfFoundry";
+        return result;
     }
 }
 
