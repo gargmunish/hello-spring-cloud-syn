36ba4e151737e7a2def1a7f1e11a61c642271526
gargmunish <42627686+gargmunish@users.noreply.github.com>
2018-10-05 11:09:17 -0500

    Update HomeController.java

--- src/main/java/helloworld/HomeController.java
+++ src/main/java/helloworld/HomeController.java
@@ -1,24 +1,6 @@
 package helloworld;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-import javax.sql.DataSource;
-
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.app.ApplicationInstanceInfo;
-import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
